3a746d4e09d2153cd125bba821f8c6ef14a96d3e
DReX97 <148553511+DReX97@users.noreply.github.com>
2025-03-18 16:41:43 +0000

    Third Commit - Queue class functions

--- src/btschedulerapp/MyQueue.java
+++ src/btschedulerapp/MyQueue.java
@@ -3,11 +3,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package btschedulerapp;
-
+import java.util.ArrayList;
 /**
  *
  * @author damie
  */
-public class MyQueue {
-    
+public class MyQueue implements QueueInterface{
+        //ArrayList to hold the queue items (I used this for no-show/absents)
+    private ArrayList<String> theQueue;
+
+    //Constructor
+    public MyQueue() {
+        theQueue = new ArrayList<String>(); // Initialize the queue as empty.
+    }
+
+    //checks if the queue is empty.
+    public boolean isEmpty() {
+        return theQueue.isEmpty(); // Returns true if there are no elements.
+    }
+
+    //returns the size of the queue.
+    public int size() {
+        return theQueue.size();
+    }
+
+    // Returns the element at the front of the queue without removing it.
+    public Object frontElement() {
+        if (theQueue.size() > 0) { //Checks if the queue has any elements.
+            return theQueue.get(0); //Return the first element.
+        } else {
+            return null; //return null if it's empty
+        }
+    }
+
+    //adds new element to the queue
+    public void enqueue(Object element) {
+        //If the queue already has 5 or more items
+        if (theQueue.size() >= 5) {
+            theQueue.remove(0); //Remove the oldest element.
+        }
+        theQueue.add((String) element); //adds new element to the end of the queue.
+    }
+
+    //Removes and returns the element at the front of the queue.
+    public Object dequeue() {
+        if (theQueue.size() > 0) {
+            return theQueue.remove(0); //Remove and return the first element.
+        } else {
+            return null;
+        }
+    }
 }
--- src/btschedulerapp/QueueInterface.java
+++ src/btschedulerapp/QueueInterface.java
@@ -8,6 +8,20 @@ package btschedulerapp;
  *
  * @author damie
  */
-public class QueueInterface {
+public interface QueueInterface {
+    // Checks if the queue is empty.
+    public boolean isEmpty();
+
+    // Returns the number of elements in the queue.
+    public int size();
+
+    // Returns the element at the front of the queue without removing it.
+    public Object frontElement();
+
+    // Adds a new element to the queue.
+    public void enqueue(Object element);
+
+    // Removes and returns the element at the front of the queue.
+    public Object dequeue();
     
 }
